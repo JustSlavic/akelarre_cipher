c377e376cf57d9febd0b6ab58fd1da9fa915bdc7
radko <justslavic@gmail.com>
2018-01-15 14:04:04 +0300

    remove negation function and add casts for signed values

--- keygen.c
+++ keygen.c
@@ -49,35 +49,28 @@ uint32_t get_next_key(generator* g) {
     }
 }
 
-uint32_t neg(uint32_t a){
-//    uint32_t x = a & 0x7f;
-//    uint32_t y = -(x % 128);
-//    return (a & 0xFFFFFF80) | (y & 0x7f);
-    return -a;
-}
-
 void gen_decrypt_keys(uint16_t* master_key, uint32_t* d, uint32_t nrounds) {
     uint32_t* k = calloc(13*nrounds + 9, sizeof(uint32_t));
     gen_encrypt_keys(master_key, k, nrounds);
 
-    d[0] = -k[4 + 13*nrounds + 1];
+    d[0] = (uint32_t) -k[4 + 13*nrounds + 1];
     d[1] =  k[4 + 13*nrounds + 2];
     d[2] =  k[4 + 13*nrounds + 3];
-    d[3] = -k[4 + 13*nrounds + 4];
+    d[3] = (uint32_t) -k[4 + 13*nrounds + 4];
 
     for (int r = 0; r < nrounds; ++r) {
-        d[13*r+4] = neg(k[4 + 13*(nrounds-r)]);
+        d[13*r+4] = (uint32_t) -k[4 + 13*(nrounds-r)];
         for(int j = 5; j <= 16; ++j){
             d[13*r+j] = k[13*(nrounds - r - 1) + j];
         }
     }
 
-    d[13*nrounds + 4] = neg(k[4]);
+    d[13*nrounds + 4] = (uint32_t) -k[4];
 
-    d[13*nrounds + 5] = -k[0];
+    d[13*nrounds + 5] = (uint32_t) -k[0];
     d[13*nrounds + 6] =  k[1];
     d[13*nrounds + 7] =  k[2];
-    d[13*nrounds + 8] = -k[3];
+    d[13*nrounds + 8] = (uint32_t) -k[3];
 }
 
 void gen_encrypt_keys(uint16_t* master_key, uint32_t* k, uint32_t r) {
